d5a569e2077146a8aded9c715e486094eac9bbde
Your Name <you@example.com>
2021-04-09 17:43:42 +0200

    primo ordina

--- client/src/app/componenti/listino/listino.component.ts
+++ client/src/app/componenti/listino/listino.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { IVino } from 'src/app/interfacce/vino';
 import { DatabaseService } from 'src/app/servizi/database.service';
-
+import { OrdineComponent } from '../ordine/ordine.component';
 @Component({
   selector: 'app-listino',
   templateUrl: './listino.component.html',
@@ -14,10 +14,12 @@ export class ListinoComponent implements OnInit {
 
   public vini: IVino[] = [];
 
-  constructor(private DatabaseService: DatabaseService) {}
+  constructor(private DatabaseService: DatabaseService,private ordineComponent: OrdineComponent) {}
 
   ngOnInit(): void {
     this.DatabaseService.fetchAllVini().subscribe(data => this.vini = data, error => this.errMsg = error);
   }
-
+aggiungi (vino : IVino){
+this.ordineComponent.carrello.push(vino);
+}
 }
--- client/src/app/componenti/ordine/ordine.component.ts
+++ client/src/app/componenti/ordine/ordine.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewContainerRef } from '@angular/core';
+import { Component, OnInit, ViewContainerRef ,Input} from '@angular/core';
 import { Router } from '@angular/router';
 import { FirebaseService } from 'src/app/servizi/firebase.service';
 import { IOrdine } from '../../interfacce/ordine';
@@ -15,21 +15,36 @@ import { IVino } from 'src/app/interfacce/vino';
 
 export class OrdineComponent implements OnInit {
 
-  private vini: IVino[] = [];
-  private errMsg = "";
+ @Input('vino') public  carrello : IVino[]= [];
+
 
   constructor(private authService: FirebaseService, private router: Router, private DatabaseService: DatabaseService) {}
 
   ngOnInit(): void {
-    if (!this.authService.isLoggedIn) {
-      this.router.navigateByUrl('/login');
-    } else {
-      this.DatabaseService.fetchAllVini().subscribe(data => this.vini = data, error => this.errMsg = error);
-    }
+   // if (!this.authService.isLoggedIn) {
+     // this.router.navigateByUrl('/login');
+    //} else {
+     // this.DatabaseService.fetchAllVini().subscribe(data => this.vini = data, error => this.errMsg = error);
+    //}
   }
 
   metodo(): void {
     console.log("paolo");
   }
-
+  ordina(): void {
+    let ordine: IOrdine = {_id: '', utente: '', importo: 0, vini: []};
+    ordine.importo = 100;
+    ordine.utente = 'utentediprova@email.ao';
+    ordine.vini = ['vino1', 'vinodicasa'];
+    this.invia(ordine);
+  }
+  invia(ordine: IOrdine) {
+    this.DatabaseService.postOrdine(ordine).subscribe(
+      response => {
+        console.log('Ordine inserito!');
+        this.router.navigateByUrl('/homepage')
+      },
+      error => console.log('ERRORE:', error)
+    )
+  }
 }
